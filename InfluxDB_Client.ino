fdbc27044398b8cef29cc080ae732ba9b8eec7e3
StathisKap <stathiskap75@gmail.com>
2022-08-24 22:42:21 +0300

    Added a Function to Measure Air temperature with the DHT11 and Soil. The Soil sensor hasn't been implemented yet

--- InfluxDB_Client.ino
+++ InfluxDB_Client.ino
@@ -2,8 +2,18 @@
 #include "arduino_secrets.h"
 #include <SPI.h>
 #include <WiFiNINA_Generic.h>
+#include <DHT.h>;
 
-void write_data(char * raw_data);
+enum MATERIAL{AIR, SOIL};
+
+//Constants
+#define DHTPIN 7     // what pin we're connected to
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
+
+// Initialize the client for write_data()
+WiFiClient client;
+int status = WL_IDLE_STATUS;
 
 // please enter your sensitive data in the Secret tab/arduino_secrets.h
 char ssid[] = SECRET_SSID;        // your network SSID (name)
@@ -11,27 +21,28 @@ char pass[] = SECRET_PASS;        // your network password (use for WPA, or use 
 char server[]               =   "192.168.2.24";
 int  port                   =    8086;
 char path_and_querry[]      =   "/api/v2/write?bucket=Front_Garden/";
-char *data_raw = (char*)malloc(155 * sizeof(char));
 
-WiFiClient client; // Initialize the client for write_data()
-int status = WL_IDLE_STATUS;
+// Global Variables
+float soil_temp;
+float soil_humidity;
+
+
+// Prototypes
+void write_data(char * raw_data);
+void Measure_Humidity_and_Temp(enum MATERIAL);
+
 
 void setup()
 {
-  float soil_temp = 27.72;
-  float soil_humidity = 55.5;
-  char soil_temp_str[9];
-  char soil_humidity_str[9];
-
-  dtostrf(soil_temp,5,3,soil_temp_str);
-  dtostrf(soil_humidity,5,3,soil_humidity_str);
-  sprintf(data_raw, "%s,source=garduino temp=%s,humidity=%s", "soil", soil_temp_str, soil_humidity_str);
-  write_data(data_raw);
+  Serial.begin(9600);
+  dht.begin();
 }
 
 void loop()
 {
-  delay(5000);
+  Measure_Humidity_and_Temp(SOIL);
+  Measure_Humidity_and_Temp(AIR);
+  delay(10000); //Delay 2 sec.
 }
 
 void write_data(char *raw_data)
@@ -64,5 +75,37 @@ void write_data(char *raw_data)
     client.println(String("Content-Length: ") + String(strlen(raw_data)));
     client.println();
     client.write(raw_data);
+    delay(5000);
+    client.stop();
   }
+}
+
+void Measure_Humidity_and_Temp(enum MATERIAL type)
+{
+  float humidity;
+  float temp;
+  char material[5];
+
+  if (type == AIR)
+  {
+    //Read data and store it to variables humidity and temp
+    humidity  = dht.readHumidity();
+    temp      = dht.readTemperature();
+    strcpy(material,"air");
+  }
+  else if (type == SOIL)
+  {
+    humidity  = dht.readHumidity() - 2;
+    temp      = dht.readTemperature() - 2;
+    strcpy(material,"soil");
+  }
+
+  char  temp_str[9];
+  char  humidity_str[9];
+  dtostrf(temp,5,3,temp_str);
+  dtostrf(humidity,5,3,humidity_str);
+
+  char *data_raw = (char*)malloc(155 * sizeof(char));
+  sprintf(data_raw, "%s,source=garduino temp=%s,humidity=%s", material, temp_str, humidity_str);
+  write_data(data_raw);
 }
